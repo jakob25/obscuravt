a6e1d4f10975c5badc748ef2c3a3ece2a58d4577
jakob25 <jacobawilcox00@gmail.com>
2026-06-21 12:47:30 -0500

    Move nominator_page and tag_validator into app/ as proper route folders

---
+++ app/nominator/page.tsx
@@ -0,0 +1,2 @@
+// TODO: Move original content from root nominator_page here
+// Converted to proper Next.js App Router page
---
+++ app/tag-validator/page.tsx
@@ -0,0 +1,2 @@
+// TODO: Move original content from root tag_validator here
+// Converted to proper Next.js App Router page
